e80475bf16b5aabf88acf45dc6fa219f6b858557
Magagnin <magagningu@gmail.com>
2025-10-28 09:14:21 -0300

    Add DomainException class for exception handling

--- Exceptions/DomainException.cs
+++ Exceptions/DomainException.cs
@@ -1,1 +1,10 @@
+using System;
+
+namespace OSLite.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        protected DomainException(string message) : base(message) { }
+    }
+}
 
