314cc03ac62936491082d0b82948134e45e71ef8
Magagnin <magagningu@gmail.com>
2025-10-28 09:18:10 -0300

    Add unit test for adding order to client

--- Tests/ClienteTests.cs
+++ Tests/ClienteTests.cs
@@ -1,1 +1,22 @@
+using Xunit;
+using OSLite.Domain;
+using OSLite.Domain.ValueObjects;
+
+namespace OSLite.Tests
+{
+    public class ClienteTests
+    {
+        [Fact]
+        public void DeveAdicionarOrdemAoCliente()
+        {
+            var cliente = new Cliente("João", new Email("joao@teste.com"));
+            var ordem = new OrdemDeServico();
+
+            cliente.AdicionarOrdem(ordem);
+
+            Assert.Single(cliente.Ordens);
+            Assert.Equal(cliente, ordem.Cliente);
+        }
+    }
+}
 
