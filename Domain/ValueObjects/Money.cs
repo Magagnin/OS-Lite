3de9a81621afd48eeb04b0a81e5804cf538d30fb
Magagnin <magagningu@gmail.com>
2025-10-28 09:09:35 -0300

    Implement Money value object with validation

--- Domain/ValueObjects/Money.cs
+++ Domain/ValueObjects/Money.cs
@@ -1,1 +1,21 @@
+using OSLite.Exceptions;
+
+namespace OSLite.Domain.ValueObjects
+{
+    public readonly record struct Money
+    {
+        public decimal Valor { get; }
+
+        public Money(decimal valor)
+        {
+            if (valor < 0)
+                throw new ValorInvalidoException(valor);
+
+            Valor = valor;
+        }
+
+        public static Money operator +(Money a, Money b) => new(a.Valor + b.Valor);
+        public override string ToString() => Valor.ToString("C2");
+    }
+}
 
