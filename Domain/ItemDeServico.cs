2c259bbbb3e1c2775e179aa8626a71240c1fbdca
Magagnin <magagningu@gmail.com>
2025-10-28 09:11:16 -0300

    Implement ItemDeServico class with properties
    Added properties and constructor for ItemDeServico class.

--- Domain/ItemDeServico.cs
+++ Domain/ItemDeServico.cs
@@ -1,1 +1,20 @@
+using System;
+using OSLite.Domain.ValueObjects;
+
+namespace OSLite.Domain
+{
+    public class ItemDeServico
+    {
+        public Guid Id { get; }
+        public string Descricao { get; private set; }
+        public Money Preco { get; private set; }
+
+        public ItemDeServico(string descricao, Money preco)
+        {
+            Id = Guid.NewGuid();
+            Descricao = descricao ?? throw new ArgumentNullException(nameof(descricao));
+            Preco = preco;
+        }
+    }
+}
 
