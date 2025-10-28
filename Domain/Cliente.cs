5aba295ee2ca8ccc09d22f6c68b3e8db76b0af58
Magagnin <magagningu@gmail.com>
2025-10-28 09:10:54 -0300

    Add Cliente class with properties and methods

--- Domain/Cliente.cs
+++ Domain/Cliente.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Collections.Generic;
+using OSLite.Domain.ValueObjects;
+
+namespace OSLite.Domain
+{
+    public class Cliente
+    {
+        public Guid Id { get; }
+        public string Nome { get; private set; }
+        public Email Email { get; private set; }
+
+        private readonly List<OrdemDeServico> _ordens = new();
+        public IReadOnlyCollection<OrdemDeServico> Ordens => _ordens.AsReadOnly();
+
+        public Cliente(string nome, Email email)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            Email = email;
+        }
+
+        public void AdicionarOrdem(OrdemDeServico ordem)
+        {
+            if (ordem == null)
+                throw new ArgumentNullException(nameof(ordem));
+
+            _ordens.Add(ordem);
+            ordem.AtribuirCliente(this);
+        }
+    }
+}
 
