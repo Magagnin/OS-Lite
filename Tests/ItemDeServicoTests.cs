df1194985fc0a5cb27349e1654ed7c31b2985426
Magagnin <magagningu@gmail.com>
2025-10-28 09:20:51 -0300

    Add test for ItemDeServico creation
    Added a test to verify correct creation of ItemDeServico.

--- Tests/ItemDeServicoTests.cs
+++ Tests/ItemDeServicoTests.cs
@@ -1,1 +1,18 @@
+using Xunit;
+using OSLite.Domain;
+using OSLite.Domain.ValueObjects;
+
+namespace OSLite.Tests
+{
+    public class ItemDeServicoTests
+    {
+        [Fact]
+        public void DeveCriarItemCorretamente()
+        {
+            var item = new ItemDeServico("Troca de bateria", new Money(150));
+            Assert.Equal("Troca de bateria", item.Descricao);
+            Assert.Equal(150, item.Preco.Valor);
+        }
+    }
+}
 
