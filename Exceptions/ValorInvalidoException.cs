05ce985a847092bf7fc19e91efe1172a78a1bfd3
Magagnin <magagningu@gmail.com>
2025-10-28 09:16:08 -0300

    Add ValorInvalidoException for invalid value handling

--- Exceptions/ValorInvalidoException.cs
+++ Exceptions/ValorInvalidoException.cs
@@ -1,1 +1,9 @@
+namespace OSLite.Exceptions
+{
+    public class ValorInvalidoException : DomainException
+    {
+        public ValorInvalidoException(decimal valor)
+            : base($"Valor inválido: {valor}") { }
+    }
+}
 
